e035d6b5ff3aa14c62316d4798351541797bb9cf
SharpyShibe <github.q1kx6@simplelogin.fr>
2025-02-03 08:02:28 +0100

    Leetcode Daily 0009 c/cpp

---
+++ 0009-Palindrome-Number/0009.c
@@ -0,0 +1,18 @@
+//#include <stdbool.h>
+
+bool isPalindrome(int x) {
+
+	if(x < 0) return false;
+	if(x == 0) return true; 
+
+	int num = x; 
+	long rev = 0;
+
+	while(num > 0) {
+		int digit = num % 10; 
+		rev = rev * 10 + digit;
+		num /= 10; 
+	}
+
+	return x == rev;
+}
---
+++ 0009-Palindrome-Number/0009.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if(x < 0) return false;
+        if (x == 0) return true;
+
+        int num = x; 
+        long rev = 0;
+
+        while(num > 0) {
+            int digit = num % 10; 
+            rev = rev * 10 + digit; 
+            num /= 10;
+        }
+
+        return x == rev;
+    }
+};
