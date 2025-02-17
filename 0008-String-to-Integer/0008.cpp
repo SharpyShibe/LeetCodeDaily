1715297de496f8828558ef09b4c9c201156587f7
SharpyShibe <github.q1kx6@simplelogin.fr>
2025-02-17 11:24:12 +0100

    Leetcode Daily 0008 cpp

---
+++ 0008-String-to-Integer/0008.cpp
@@ -0,0 +1,64 @@
+#include <ctype.h>
+#include <limits.h>
+#include <string>
+
+using namespace std;
+
+class Solution {
+
+private:
+    void  extractNumber(const string& s, bool& neg, int& start, int& end) {
+        start = 0; 
+        end = 0; 
+        neg = false;
+
+        while(start < s.length() && s.at(start) == ' ') start++;
+
+        if(start >= s.length()) return;
+
+        char c = s.at(start);
+
+        if(c=='+') 
+            start++;
+        else if(c == '-') {
+            start++;
+            neg = true;
+        } else if(!isdigit(c)) {
+            start = -1;
+            return;
+        }
+
+        while(start < s.length() && s.at(start)=='0') start++;
+
+        end = start;
+
+        while(end < s.length() && isdigit(s.at(end))) end++;
+        if(end < s.length() && !isdigit(s.at(end))) end--;
+        if(end >= s.length()) end = s.length()-1;
+
+
+     }
+
+
+public:
+    int myAtoi(string s) {
+        int start, end; 
+        bool neg;
+
+        extractNumber(s, neg, start, end);
+
+        if(start >= s.length() || !isdigit(s.at(start)) || end >= s.length()) return 0;
+        
+        long result = 0;
+
+        for(int i=start; i<= end; i++) {
+            result = result*10 + (int)(s.at(i)-'0');
+
+            if(!neg && result > INT_MAX) return INT_MAX;
+            if(neg && -result < INT_MIN) return INT_MIN;
+        }
+
+        return neg? -result : result;
+    }
+
+};
